cedcfc8963bccad83e348af089cbe8bd936f0684
William Teles <teles967@gmail.com>
2021-06-12 20:41:19 -0300

    Creating Serializers of models

--- account_service/account/serializers.py
+++ account_service/account/serializers.py
@@ -1,8 +1,37 @@
+from typing import ClassVar
+
+from django.db import models
+from django.db.models import fields
 from rest_framework.serializers import ModelSerializer
-from .models import Account
+from .models import Account, Card, LoanCredit, AccountType, Transactions, TransactionsType
 
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
-        fields = ['account_number', 'balance', 'owner_id']
+        fields = ['__all__']
+
+class AccountTypeSerializer(ModelSerializer): 
+    class Meta:
+        model = AccountType
+        fields = ['__all__']
+
+class LoanCreditSerializer(ModelSerializer):
+    class Meta:
+        model = LoanCredit
+        fields = ['__all__']
+
+class CardSerializer(ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['__all__']
+
+class TransactionsSerializer(ModelSerializer):
+    class Meta:
+        model = Transactions
+        fields = ['__all__']
+
+class TransactionsTypeSerializer(ModelSerializer):
+    class Meta:
+        model = TransactionsType
+        fields ['__all__']
