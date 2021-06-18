be62f603514b0ac8f4314c32f2ce085a482bb599
William Teles <teles967@gmail.com>
2021-06-18 01:34:11 -0300

    Fixing bugs and refactoring some serializers

--- account_service/account/serializers.py
+++ account_service/account/serializers.py
@@ -1,29 +1,38 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Account, Card, AccountType, Transactions, TransactionsType
+from .models import Account, Card, Transactions
 
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
-        fields = ['__all__']
+        fields = '__all__'
+
 
 class AccountTypeSerializer(ModelSerializer): 
     class Meta:
-        model = AccountType
-        fields = ['__all__']
+        model = Account
+        fields = ['id', 'type_account']
+
+
+class AccountSimpleSerializer(ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
 
 
 class CardSerializer(ModelSerializer):
     class Meta:
         model = Card
-        fields = ['__all__']
+        fields = '__all__'
+
 
 class TransactionsSerializer(ModelSerializer):
     class Meta:
         model = Transactions
-        fields = ['__all__']
+        fields = '__all__'
+
 
 class TransactionsTypeSerializer(ModelSerializer):
     class Meta:
-        model = TransactionsType
-        fields = ['__all__']
+        model = Transactions
+        fields = ['id', 'type_transaction']
