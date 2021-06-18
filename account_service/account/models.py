27912741a67f3d02f1a4fb39ba3a3cb630d5731c
William Teles <teles967@gmail.com>
2021-06-18 01:33:44 -0300

    Refactoring models and fixing some bugs

--- account_service/account/models.py
+++ account_service/account/models.py
@@ -1,52 +1,32 @@
 from django.db import models
 
 
-class AccountType(models.Model):
+class Account(models.Model):
     TYPE = (
         ('poupanca', 'Conta Poupança'),
         ('corrente', 'Conta Corrente'),
         ('salario', 'Conta Salário')
     )
 
-    type = models.CharField(max_length=50, choices=TYPE)
-
-    def __str__(self):
-        return f"{self.type}"
-
-
-class Account(models.Model):
     account_number = models.CharField(max_length=10, unique=True)
     balance = models.DecimalField(max_digits=18, decimal_places=10)
     owner_id = models.IntegerField(unique=True)
-    type_account = models.ForeignKey(AccountType, on_delete=models.PROTECT, null=True)
+    type_account = models.CharField(max_length=50, choices=TYPE)
 
 
 class Card(models.Model):
-    card_number = models.CharField(max_length=16, unique=True)
+    card_number = models.CharField(max_length=19, unique=True)
     expire_date = models.DateField(blank=False)
     cvv = models.CharField(max_length=3)
-    password = models.PositiveIntegerField(max_length=4, blank=False, null=False)
-    has_debit = models.BooleanField(default=False)
+    password = models.PositiveIntegerField(blank=False, null=False)
+    has_debit = models.BooleanField(default=True)
     has_credit = models.BooleanField(default=False)
     is_blocked = models.BooleanField(default=False)
     bill = models.DecimalField(max_digits=18, decimal_places=10)
     limit = models.DecimalField(max_digits=18, decimal_places=10)
     account = models.ForeignKey(Account, on_delete=models.PROTECT)
 
-
-class TransactionsType(models.Model):
-    TYPE = (
-        ('deposito', 'Depósito'),
-        ('pagamento', 'Pagamento'),
-        ('transferencia', 'Transferencia'),
-        ('compra', 'Compra')
-    )
-
-    type = models.CharField(max_length=50, choices=TYPE)
-
-    # def __str__(self):
-    #     return f"{self.type}"
-    
+   
 class Transactions(models.Model):
     CATEGORIES = (
         ('restaurante', 'Restaurante'),
@@ -61,9 +41,16 @@ class Transactions(models.Model):
         ('outros', 'Outros')
     )
 
+    TYPE = (
+        ('deposito', 'Depósito'),
+        ('pagamento', 'Pagamento'),
+        ('transferencia', 'Transferencia'),
+        ('compra', 'Compra')
+    )
+
     date = models.DateTimeField(blank=False)
     value = models.DecimalField(max_digits=18, decimal_places=10)
-    parcelas = models.PositiveIntegerField(max_length=3)
+    parcelas = models.PositiveIntegerField()
     categories = models.CharField(max_length=50, choices=CATEGORIES)
-    type_transaction = models.ForeignKey(TransactionsType, on_delete=models.PROTECT, null=True)
+    type_transaction = models.CharField(max_length=50, choices=TYPE)
     card = models.ForeignKey(Card, on_delete=models.PROTECT)
