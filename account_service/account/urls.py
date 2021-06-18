4f9575d04c17f6bd476b0be669d3293689d78f91
William Teles <teles967@gmail.com>
2021-06-18 01:34:39 -0300

    Creating path urls to views

--- account_service/account/urls.py
+++ account_service/account/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-# from .views import AccountView
+from .views import AccountDetail, AccountTypeReader, AccountList, CardDetail, CardList, TransactionsDetail, TransactionsList, TransactionsTypeReader
 
 
-# urlpatterns = [
-#     path("", AccountView.as_view()),
-# ]
+urlpatterns = [
+    path(r"account/", AccountList.as_view()),
+    path(r"account/<int:pk>/", AccountDetail.as_view()),
+    path(r"account-type/<int:pk>/", AccountTypeReader.as_view()),
+    path(r"card/", CardList.as_view()),
+    path(r"card/<int:pk>", CardDetail.as_view()),
+    path(r"transactions/", TransactionsList.as_view()),
+    path(r"transactions/<int:pk>/", TransactionsDetail.as_view()),
+    path(r"transactions-type/<int:pk>/", TransactionsTypeReader.as_view())
+]
