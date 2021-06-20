d135ba720d92901c37e97c501dc5cb710fb29440
William Teles <teles967@gmail.com>
2021-06-20 01:26:14 -0300

    creating path to update balance and bill

--- account_service/account/urls.py
+++ account_service/account/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import AccountDetail, AccountList, CardDetail, CardList, TransactionsDetail, TransactionsList
+from .views import AccountDetail, AccountList, AccountUpdateBalance, CardDetail, CardList, CardUpdateBill, TransactionsDetail, TransactionsList
 
 
 urlpatterns = [
     path(r"account/", AccountList.as_view()),
     path(r"account/<int:pk>/", AccountDetail.as_view()),
     path(r"card/", CardList.as_view()),
-    path(r"card/<int:pk>", CardDetail.as_view()),
+    path(r"card/<int:pk>/", CardDetail.as_view()),
     path(r"transactions/", TransactionsList.as_view()),
-    path(r"transactions/<int:pk>/", TransactionsDetail.as_view())
+    path(r"transactions/<int:pk>/", TransactionsDetail.as_view()),
+    path(r"cardbill/<int:pk>/", CardUpdateBill.as_view()),
+    path(r"accountbalance/<int:pk>/", AccountUpdateBalance.as_view())
 ]
