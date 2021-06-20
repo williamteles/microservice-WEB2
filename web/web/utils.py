079813f3de5d3e845d735b9830340a7cb801bcba
William Teles <teles967@gmail.com>
2021-06-19 23:40:51 -0300

    Updating some utils and creating a new to transactions creation

--- web/web/utils.py
+++ web/web/utils.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime
 import requests
 
 
@@ -13,6 +14,8 @@ def get_account_from_owner(owner_id):
     accounts = response.json()
 
     if response.status_code == 200:
+        if len(accounts) == 0:
+            return accounts
 
         for account in accounts:
             if account["owner_id"] == owner_id:
@@ -27,16 +30,22 @@ def get_card_from_account(account_id):
     cards = response.json()
 
     if response.status_code == 200:
-    
+        if len(cards) == 0:
+            return cards
+        
         for card in cards:
             if card["account"] == account_id:
+                expire_date_obj = datetime.strptime(card["expire_date"], '%Y-%m-%d')
+                expire_date = datetime.strftime(expire_date_obj, '%m/%Y')
+                card["expire_date"] = expire_date
+
                 return card        
     else:
         error = {"has_error": True, "error_message": cards["message"]}
         return error
 
 
-def get_transactions_from_card(card):
+def get_transactions_from_account(account_id):
     response = requests.get("http://account-api:8000/acct/transactions/")
     transactions = response.json()
     
@@ -44,10 +53,21 @@ def get_transactions_from_card(card):
         account_transactions =  []
         
         for transaction in transactions:
-            if transaction["card"] == card:
+            if transaction["account"] == account_id:
+
                 account_transactions.append(transaction)
         
         return account_transactions
     else:
         error = {"has_error": True, "error_message": transactions["message"]}
         return error
+
+def create_transaction(body):
+    api_response = requests.post(f"http://account-api:8000/acct/transactions/", json=body)
+    payload = api_response.json()
+
+    if api_response.status_code is not 201:
+        error = {"has_error": True, "error_message": payload["message"]}
+        return error
+    else:
+        return "Created"
