47a57b0dbcf1c684e7b384464523766765f56836
William Teles <teles967@gmail.com>
2021-06-20 01:25:38 -0300

    Creating view to update balance and bill

--- account_service/account/views.py
+++ account_service/account/views.py
@@ -1,5 +1,5 @@
 from .models import Account, Card, Transactions
-from .serializers import AccountSerializer, CardSerializer, TransactionsSerializer
+from .serializers import AccountSerializer, AccountUpdateBalanceSerializer, CardSerializer, CardUpdateBillSerializer, TransactionsSerializer
 from django.http import Http404
 from rest_framework import status
 from rest_framework import mixins
@@ -210,6 +210,37 @@ class TransactionsList(mixins.ListModelMixin,
     def post(self, request, *args, **kwargs):
         try:
             response = self.create(request, *args, **kwargs)
+        except ValidationError as e:
+            error_message = generate_error_message(e.detail)
+            response = Response({"message": error_message}, status=e.status_code)
+
+        return response
+
+
+class CardUpdateBill(mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardUpdateBillSerializer
+
+    def put(self, request, *args, **kwargs):
+        try:
+            response = self.update(request, *args, **kwargs)
+        except Http404 as e:
+            response = Response({"message": str(e)}, status=status.HTTP_404_NOT_FOUND)
+        except ValidationError as e:
+            error_message = generate_error_message(e.detail)
+            response = Response({"message": error_message}, status=e.status_code)
+
+        return response
+
+class AccountUpdateBalance(mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountUpdateBalanceSerializer
+
+    def put(self, request, *args, **kwargs):
+        try:
+            response = self.update(request, *args, **kwargs)
+        except Http404 as e:
+            response = Response({"message": str(e)}, status=status.HTTP_404_NOT_FOUND)
         except ValidationError as e:
             error_message = generate_error_message(e.detail)
             response = Response({"message": error_message}, status=e.status_code)
