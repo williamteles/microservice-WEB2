9fc4b8924a1bc92543e9cb8c2d1eb4c9f6852993
Gabriel Lima <gabriellima.glgs@gmail.com>
2021-06-25 22:41:13 -0300

    add mask money

--- web/static/js/mask.js
+++ web/static/js/mask.js
@@ -10,3 +10,10 @@ $(document).ready(function(){
 })
 
 //maks dinero tag <p>
+var tag_numero = document.querySelectorAll('.money');
+
+for (var i = 0; i < tag_numero.length; i++){
+    var numero = parseFloat(tag_numero[i].innerText);
+    var moneyBRL = numero.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    tag_numero[i].innerText = moneyBRL;
+}
